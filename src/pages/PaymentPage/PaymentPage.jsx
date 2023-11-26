4e380bfeae2442796c9b1289a1ad8ee41ec822d8
HieuNT22012296 <nguyentrunghieu01082001@gmail.com>
2023-11-26 14:42:04 +0700

    update pakage

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -14,7 +14,7 @@ import Loading from '../../components/LoadingComponent/Loading';
 import { updateUser } from '../../redux/slides/userSlide';
 import { useNavigate } from 'react-router-dom';
 import { removeAllOrderProduct } from '../../redux/slides/orderSlide';
-import { PayPalButton } from "@paypal/react-paypal-js";
+import { PayPalButton } from "react-paypal-button-v2";
 import * as PaymentService from '../../services/PaymentService'
 
 
